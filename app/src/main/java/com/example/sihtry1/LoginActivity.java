21f70ceedd8c1149a0b7ee4855aebf08c17b9dcb
pranav1999 <pranav0281999@gmail.com>
2019-02-21 00:18:10 +0530

    Adds login functionality

--- app/src/main/java/com/example/sihtry1/LoginActivity.java
+++ app/src/main/java/com/example/sihtry1/LoginActivity.java
@@ -1,18 +1,33 @@
 package com.example.sihtry1;
 
 import android.content.Intent;
+import android.content.pm.LabeledIntent;
+import android.os.PatternMatcher;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button login_btn_login;
     private Button login_btn_register;
-    private EditText login_tv_email;
-    private EditText login_tv_password;
+    private EditText login_et_email;
+    private EditText login_et_password;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +36,11 @@ public class LoginActivity extends AppCompatActivity {
 
         login_btn_login = (Button) findViewById(R.id.login_btn_login);
         login_btn_register = (Button) findViewById(R.id.login_btn_register);
-        login_tv_email = (EditText) findViewById(R.id.login_tv_email);
-        login_tv_password = (EditText) findViewById(R.id.login_tv_password);
+        login_et_email = (EditText) findViewById(R.id.login_et_email);
+        login_et_password = (EditText) findViewById(R.id.login_et_password);
+
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
 
         login_btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +48,56 @@ public class LoginActivity extends AppCompatActivity {
                 register();
             }
         });
+        login_btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        String email, password;
+        email = login_et_email.getText().toString().trim();
+        password = login_et_password.getText().toString();
+
+        if (email.isEmpty()) {
+            login_et_email.setError("Cannot be empty");
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            login_et_email.setError("Not Valid Email");
+            return;
+        }
+        if (password.isEmpty() || password.length() < 6) {
+            login_et_password.setError("Must be 6 or more character long");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("LoginActivity.java", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            loginUpdateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("LoginActivity.java", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void loginUpdateUI(FirebaseUser user) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("userobject", user);
+        startActivity(intent);
+        finish();
     }
 
     private void register() {
@@ -37,6 +105,5 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-
 
 }
--- app/src/main/java/com/example/sihtry1/RegisterActivity.java
+++ app/src/main/java/com/example/sihtry1/RegisterActivity.java
@@ -3,16 +3,18 @@ package com.example.sihtry1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private Button register_btn_login;
     private Button register_btn_register;
-    private EditText register_tv_email;
-    private EditText register_tv_password;
+    private EditText register_et_email;
+    private EditText register_et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +23,8 @@ public class RegisterActivity extends AppCompatActivity {
 
         register_btn_login = (Button) findViewById(R.id.register_btn_login);
         register_btn_register = (Button) findViewById(R.id.register_btn_register);
-        register_tv_email = (EditText) findViewById(R.id.register_tv_email);
-        register_tv_password = (EditText) findViewById(R.id.register_tv_password);
+        register_et_email = (EditText) findViewById(R.id.register_et_email);
+        register_et_password = (EditText) findViewById(R.id.register_et_password);
 
         register_btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +32,32 @@ public class RegisterActivity extends AppCompatActivity {
                 login();
             }
         });
+        register_btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+    }
+
+    private void register() {
+        String email, password;
+        email = register_et_email.getText().toString().trim();
+        password = register_et_password.getText().toString();
+
+        if (email.isEmpty()) {
+            register_et_email.setError("Cannot be empty");
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            register_et_email.setError("Not Valid Email");
+            return;
+        }
+        if (password.isEmpty() || password.length() < 6) {
+            register_et_password.setError("Must be 6 or more character long");
+            return;
+        }
+        Toast.makeText(getApplicationContext(), "sdfgfg", Toast.LENGTH_SHORT).show();
     }
 
     private void login() {
