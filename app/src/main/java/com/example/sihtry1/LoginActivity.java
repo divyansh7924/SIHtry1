7b1d35c18680fe11ff1bea0be6c09b8c5414c084
Anurag <anuragmy2729@gmail.com>
2019-02-24 03:30:44 +0530

    Update LoginActivity.java

--- app/src/main/java/com/example/sihtry1/LoginActivity.java
+++ app/src/main/java/com/example/sihtry1/LoginActivity.java
@@ -103,7 +103,6 @@ public class LoginActivity extends AppCompatActivity {
                                 .setTitle("Oops")
                                 .setMessage("Authentication failed")
                                 .setPositiveButton(android.R.string.ok, null)
-                                .setIcon(android.R.drawable.ic_dialog_alert)
                                 .show();
                              }
                         }
