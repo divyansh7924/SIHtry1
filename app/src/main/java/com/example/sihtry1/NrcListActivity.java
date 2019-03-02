cd128b4f6aa251875f858eb2ab8be071c86f6d27
divyansh7924 <guptadivyansh49@gmail.com>
2019-03-03 01:03:59 +0530

    nrc displayed

--- app/src/main/java/com/example/sihtry1/NrcListActivity.java
+++ app/src/main/java/com/example/sihtry1/NrcListActivity.java
@@ -6,48 +6,40 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sihtry1.models.NRC;
 import com.example.sihtry1.models.RCR;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
-public class CitiesActivity extends AppCompatActivity {
+public class NrcListActivity extends AppCompatActivity {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference cityref = db.collection("nrc");
-
-    private CityAdapter adapter;
+    private CollectionReference nrcref = db.collection("nrc");
     String str;
-    String selectedcity;
+
+    private NrcListAdapter adapter;
+//    String stateselected;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cities);
-//        Bundle bundle = getIntent().getExtras();
-//        String message = bundle.getString("message");
+        setContentView(R.layout.activity_nrc_list);
+
         Intent intent = getIntent();
         str = intent.getStringExtra("message");
-        Toast.makeText(CitiesActivity.this, "str" + str, Toast.LENGTH_LONG).show();
-        setupRecyclerView();
-        adapter.setOnItemClickListener(new CityAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
-                NRC city = documentSnapshot.toObject(NRC.class);
-                String id = documentSnapshot.getId();
-                selectedcity = city.getCity();
-                Toast.makeText(CitiesActivity.this,"position: " + position + "ID: " + id + selectedcity, Toast.LENGTH_SHORT).show();
-                shownrcs();
+        Toast.makeText(NrcListActivity.this, "str" + str, Toast.LENGTH_LONG).show();
 
-            }
-        });
+        setupRecyclerView();
+
 
     }
 
@@ -55,18 +47,18 @@ public class CitiesActivity extends AppCompatActivity {
         String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
 
+        Query query = nrcref.whereEqualTo("city", str).orderBy("title",Query.Direction.ASCENDING);
 
-        //Query query1 = cityref.orderBy("city", Query.Direction.ASCENDING);
-        Query query = cityref.whereEqualTo("state", str).orderBy("city",Query.Direction.ASCENDING);
+
         FirestoreRecyclerOptions<RCR> options = new FirestoreRecyclerOptions.Builder<RCR>()
                 .setQuery(query, RCR.class)
                 .build();
 
 
-        adapter = new CityAdapter(options);
+        adapter = new NrcListAdapter(options);
 
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerviewcity);
+        RecyclerView recyclerView = findViewById(R.id.recyclerviewnrc);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
@@ -83,9 +75,5 @@ public class CitiesActivity extends AppCompatActivity {
         super.onStop();
         adapter.stopListening();
     }
-    public void shownrcs(){
-        Intent intent = new Intent(this, NrcListActivity.class);
-        intent.putExtra("message", selectedcity);
-        startActivity(intent);
-    }
 }
+
