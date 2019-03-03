9e0935659c1791568b081742eead96b12477b6e0
divyansh7924 <guptadivyansh49@gmail.com>
2019-03-03 08:35:39 +0530

    merge ready

--- app/src/main/java/com/example/sihtry1/NRCActivity.java
+++ app/src/main/java/com/example/sihtry1/NRCActivity.java
@@ -1,13 +1,46 @@
 package com.example.sihtry1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NRCActivity extends AppCompatActivity {
+
+    private Button nrc_current_referrals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nrc);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        switch (item.getItemId())
+        {
+            case R.id.main_menu_signout:
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
